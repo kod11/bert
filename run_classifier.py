81c2dae107d6d97c220ea1527c58b73f51c4398c
kod11 <rickard.kodet@gmail.com>
2019-02-14 14:43:49 +0100

    Update run_classifier.py
    ikjkj

--- run_classifier.py
+++ run_classifier.py
@@ -204,6 +204,41 @@ class DataProcessor(object):
       return lines
 
 
+#FLAG MY OWN HILLARY STANCE DETECTION
+class HillaryStanceProcessor(DataProcessor):
+	"""proccessor for the homemade hillary stance dataset."""
+  
+  def get_train_examples(self, data_dir):
+	return self._create_examples(
+          self._read_tsv(os.path.join(data_dir, "hillaryTrain.tsv")), "train")
+
+  def get_dev_examples(self, data_dir):
+	return self._create_examples(
+	  self._read_tsv(os.path.join(data_dir, "hillaryDev.tsv")), "dev")
+
+  def get_test_examples(self, data_dir):
+	return self._create_examples(
+	  self._read_tsv(os.path.join(data_dir, "hillaryTest.tsv")), "test")
+
+  def get_labels(self):
+    return ["FAVOR","AGAINST","NONE"]  
+
+  def _create_examples(self, lines, set_type):
+  """creates examples for the training and dev set"""
+    examples = []
+    for (i,line) in enumerate(lines):
+      guid = "%s-%s" % (set_type, i)
+      if set_type == "test":
+        text_a = tokenization.convert_to_unicode(line[1])
+	#TODO: not sure what this label should be for test
+	label = "AGAINST"
+      else:
+	text_a = tokenization.convert_to_unicode(line[1])
+	label = tokenization.convert_to_unicode(line[2])
+       examples.append(
+	InputExample(guid=guid,text_a=text_a, text_b=None,label=label))
+    return examples 
+
 class XnliProcessor(DataProcessor):
   """Processor for the XNLI data set."""
 
@@ -788,6 +823,7 @@ def main(_):
       "mnli": MnliProcessor,
       "mrpc": MrpcProcessor,
       "xnli": XnliProcessor,
+      "hillaryStance": HillaryStanceProcessor,
   }
 
   tokenization.validate_case_matches_checkpoint(FLAGS.do_lower_case,
